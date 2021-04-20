b5f0cbb3381125644ff2938d422dbc5d64dc5c49
yzjy19 <56280386+yzjy19@users.noreply.github.com>
2021-04-20 17:09:41 -0400

    connect to database
    front end index.js setup
    connect to mongodb cloud cluster

--- server/index.js
+++ server/index.js
@@ -2,3 +2,18 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(express.json({ limit: "30mb", extended: "true" }));
+app.use(express.urlencoded({ limit: "30mb", extended: "true" }));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://yzjy19:1234567890@cluster0.02aox.mongodb.net/memories?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => app.listen(PORT, () => console.log(`server running on port ${PORT}`)))
+  .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
