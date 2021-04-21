2d9101cc7eff7c5e859c38aaffd443af8a9b4ba7
yzjy19 <56280386+yzjy19@users.noreply.github.com>
2021-04-20 21:51:39 -0400

    contollers setup
    setup the controllers for mvc model

--- server/index.js
+++ server/index.js
@@ -2,8 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import postRoutes from './routes/posts.js';
 
 const app = express();
+
+app.use('/posts', postRoutes);
 
 app.use(express.json({ limit: "30mb", extended: "true" }));
 app.use(express.urlencoded({ limit: "30mb", extended: "true" }));
