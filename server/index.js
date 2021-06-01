799526a0f24aa0ac7e2646e605fd66fd74737e84
yzjy19 <56280386+yzjy19@users.noreply.github.com>
2021-06-01 15:48:00 -0400

    environmental variable

--- server/index.js
+++ server/index.js
@@ -3,8 +3,11 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv';
+
 
 const app = express();
+dotenv.config();
 
 app.use(express.json({ limit: "30mb", extended: "true" }));
 app.use(express.urlencoded({ limit: "30mb", extended: "true" }));
@@ -12,10 +15,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = "mongodb+srv://yzjy19:1234567890@cluster0.02aox.mongodb.net/memories?retryWrites=true&w=majority"
+// const CONNECTION_URL = "mongodb+srv://yzjy19:1234567890@cluster0.02aox.mongodb.net/memories?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => app.listen(PORT, () => console.log(`server running on port ${PORT}`)))
   .catch((error) => console.log(error.message));
 
